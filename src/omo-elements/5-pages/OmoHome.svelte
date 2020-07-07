d25697c8054e9fea2f0aaeb4774f2d1764f37f02
Samuel Andert <samuelandert@me.com>
2020-07-07 20:50:00 +0200

    added first handbook docu skeleton

--- src/omo-elements/5-pages/OmoHome.svelte
+++ src/omo-elements/5-pages/OmoHome.svelte
@@ -1,10 +1,8 @@
 <script>
   import OmoFullScreenLayout from "./../4-layouts/OmoFullScreenLayout";
   import OmoStart from "./../3-organisms/OmoStart";
-  import MD from "./../1-atoms/MD";
 </script>
 
 <OmoFullScreenLayout>
   <OmoStart />
-  <MD />
 </OmoFullScreenLayout>
