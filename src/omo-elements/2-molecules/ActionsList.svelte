a7a270e5d7217d18e62f6fa4ac55e8e7ee4f1eda
jaensen <no@email.comm>
2020-07-28 19:51:15 +0200

    Added a more descriptive variant of events.

--- src/omo-elements/2-molecules/ActionsList.svelte
+++ src/omo-elements/2-molecules/ActionsList.svelte
@@ -1,21 +1,83 @@
 <script>
-function click(action) {
-    window.eventBroker.tryGetTopic("omo", "safe").publish(action);
-}
-
-let actions =  [{
-        name: "omo.safe.trust"
+    let actions =  [{
+        name: "omo.safe.trust",
+        event: () => {
+            return {
+                _$eventType: "omo.safe.trust",
+                _id: "",
+                timestamp: new Date().toJSON(),
+                data: {
+                    trustGiver: { // Owner of the trust-giving safe
+                        address: "string",
+                        privateKey: "string"
+                    },
+                    trustGiverSafe: "", // The trust-giving safe (web3.utils.toChecksumAddress(window.o.odentity.current.circleSafe.safeAddress.trim());)
+                    trustReceiverSafe: "", // The trust-receiving safe (web3.utils.toChecksumAddress(trustSafeAddress.trim()))
+                }
+            }
+        }
     },{
-        name: "omo.safe.untrust"
+        name: "omo.safe.untrust",
+        event: () => {
+            return {
+                _$eventType: "omo.safe.untrust",
+                _id: "",
+                timestamp: new Date().toJSON(),
+                data: {
+                    exTrustGiver: { // Owner of the trust-taking safe
+                        address: "string",
+                        privateKey: "string"
+                    },
+                    exTrustGiverSafe: "", // The trust-taking safe (web3.utils.toChecksumAddress(window.o.odentity.current.circleSafe.safeAddress.trim());)
+                    exTrustReceiverSafe: "", // The trust-loosing safe (web3.utils.toChecksumAddress(trustSafeAddress.trim()))
+                }
+            }
+        }
     },{
-        name: "omo.safe.transfer"
+        name: "omo.safe.transfer",
+        event: () => {
+            return {
+                _$eventType: "omo.safe.transfer",
+                _id: "",
+                timestamp: new Date().toJSON(),
+                data: {
+                    spendSafeOwner: { // Owner of the trust-taking safe
+                        address: "string",
+                        privateKey: "string"
+                    },
+                    spendSafe: "",
+                    receivingSafe: "",
+                    amount: 0.123
+                }
+            }
+        }
     },{
-        name: "omo.safe.addOwner"
+        name: "omo.safe.addOwner",
+        event: () => {
+            return {
+                _$eventType: "omo.safe.addOwner",
+                _id: "",
+                timestamp: new Date().toJSON(),
+                data: {}
+            }
+        }
     },{
-        name: "omo.safe.removeOwner"
+        name: "omo.safe.removeOwner",
+        event: () => {
+            return {
+                _$eventType: "omo.safe.removeOwner",
+                _id: "",
+                timestamp: new Date().toJSON(),
+                data: {}
+            }
+        }
     }];
+
+    function click(action) {
+        window.eventBroker.tryGetTopic("omo", "notification").publish(action);
+    }
 </script>
 hello list
 {#each actions as action}
-    <a on:click={() => click(action.name)}> {action.name} </a>
+    <a on:click={() => click(action.event())}> {action.name} </a>
 {/each}
