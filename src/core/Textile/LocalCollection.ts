d3221c62f565fef3d81c185b457d0e1f362851f2
Mama Omo <mama@omo.earth>
2020-08-04 00:28:42 +0200

    Fixed Chat Sync Part 1

--- src/core/Textile/LocalCollection.ts
+++ src/core/Textile/LocalCollection.ts
@@ -1,7 +1,7 @@
-import {Instance} from "@textile/threads-store";
-import {Collection, FilterQuery} from "@textile/hub";
-import {ICollection} from "./ICollection";
-import {Result, Database} from "@textile/threads-database";
+import { Instance } from "@textile/threads-store";
+import { Collection, FilterQuery } from "@textile/hub";
+import { ICollection } from "./ICollection";
+import { Result, Database } from "@textile/threads-database";
 
 export class LocalCollection<T extends Instance> implements ICollection<T>
 {
@@ -9,102 +9,115 @@ export class LocalCollection<T extends Instance> implements ICollection<T>
   private database: Database;
   collectionName: string;
 
-  constructor(collectionName: string, database: Database)
-  {
+  constructor(collectionName: string, database: Database) {
     this.collectionName = collectionName;
     this.database = database;
     if (!database.collections.has(collectionName)) throw new Error(`Collection "${collectionName}" not found in database`);
     this.collection = database.collections.get(collectionName) as Collection<T>;
   }
 
-  private async toArray<T>(iterator: AsyncIterable<Result<T>>): Promise<T[]>
-  {
+  private async toArray<T>(iterator: AsyncIterable<Result<T>>): Promise<T[]> {
     const arr = Array<T>();
-    for await (const entry of iterator)
-    {
+    for await (const entry of iterator) {
       arr.push(entry.value);
     }
     return arr;
   }
 
-  async all(): Promise<T[]>
-  {
+  async all(): Promise<T[]> {
     return await this.find({});
   }
 
-  async find(query: FilterQuery<T>): Promise<T[]>
-  {
+  async find(query: FilterQuery<T>): Promise<T[]> {
     var result = this.collection.find(query);
     return await this.toArray<T>(result);
   }
 
-  async findById(id: string): Promise<T>
-  {
+  async findById(id: string): Promise<T> {
     return await this.collection.findById(id);
   }
 
-  async deleteCollection(): Promise<void>
-  {
+  async deleteCollection(): Promise<void> {
     throw new Error("NotSupported");
   }
 
-  async truncate(): Promise<void>
-  {
+  async truncate(): Promise<void> {
     var ids = (await this.find({})).map(x => x._id);
     this.collection.delete(...ids);
   }
 
-  async create(value: T): Promise<T>
-  {
+  async create(value: T): Promise<T> {
     await this.collection.insert(value);
     return value;
   }
 
-  async createMany(values: T[]): Promise<T[]>
-  {
+  async createMany(values: T[]): Promise<T[]> {
     await this.collection.insert(...values);
     return values;
   }
 
-  async save(value: T): Promise<T>
-  {
+  async save(value: T): Promise<T> {
     await this.collection.save(value);
     return value;
   }
 
-  async saveMany(values: T[]): Promise<T[]>
-  {
+  async saveMany(values: T[]): Promise<T[]> {
     await this.collection.save(...values);
     return values;
   }
 
-  async delete(value: T): Promise<void>
-  {
+  async delete(value: T): Promise<void> {
     await this.collection.delete(value._id);
   }
 
-  async deleteMany(values: T[]): Promise<void>
-  {
+  async deleteMany(values: T[]): Promise<void> {
     await this.collection.delete(...values.map(x => x._id));
   }
 
-  async deleteById(value: string): Promise<void>
-  {
+  async deleteById(value: string): Promise<void> {
     await this.collection.delete(value);
   }
 
-  async deleteManyByIds(values: string[]): Promise<void>
-  {
+  async deleteManyByIds(values: string[]): Promise<void> {
     await this.collection.delete(...values);
   }
 
-  async observeUpdate(actionTypes: string[], id: string, callback: any): Promise<void>
-  {
+  async observeUpdate(actionTypes: string[], id: string, callback: any): Promise<void> {
     this.database.emitter.on(`${this.collectionName}.*.*`,
-      async (update) =>
-      {
+      async (update) => {
         console.log("UPDATE", update);
         callback(update);
       });
   }
+
+  observeAction(actionTypes: Map<string, Function>, id: string | null) {
+    actionTypes.forEach((callback, actionType, map) => {
+      let observeType = '*'; // default listen to all
+
+      switch (actionType.toLowerCase()) {
+        case "create": observeType = '0'; break;
+        case "save": observeType = '1'; break;
+        case "delete": observeType = '2'; break;
+      };
+
+      this.database.emitter.on(`${this.collectionName}.*.${observeType}`,
+        async (update) => {
+          console.log("UPDATE", update);
+          callback(update);
+        });
+    })
+  }
+}
+
+export type Update = {
+  Collection: string;
+  Type: UpdateType;
+  Id: string;
+}
+
+export enum UpdateType {
+  CREATE,
+  SAVE,
+  DELETE,
+  ALL
 }
--- src/core/Textile/SyncedThread.ts
+++ src/core/Textile/SyncedThread.ts
@@ -1,46 +1,37 @@
-import {Instance} from "@textile/threads-store";
-import {SyncedCollection} from "./SyncedCollection";
-import {RemoteThread} from "./RemoteThread";
-import {LocalThread} from "./LocalThread";
-import {JSONSchema} from "@textile/hub";
+import { Instance } from "@textile/threads-store";
+import { SyncedCollection } from "./SyncedCollection";
+import { RemoteThread } from "./RemoteThread";
+import { LocalThread } from "./LocalThread";
+import { JSONSchema } from "@textile/hub";
 
-export class SyncedThread
-{
-  private remoteThread: Promise<RemoteThread>;
+export class SyncedThread {
+  private remoteThread: RemoteThread;
   private localThread: LocalThread;
 
-  private constructor(localThread, remoteThread: Promise<RemoteThread>)
-  {
+  private constructor(localThread, remoteThread: RemoteThread) {
     this.localThread = localThread;
     this.remoteThread = remoteThread;
   }
 
-  static async init(threadName): Promise<SyncedThread>
-  {
+  static async init(threadName): Promise<SyncedThread> {
     var localThread = await LocalThread.init(threadName);
-    var remoteThread = RemoteThread.init(threadName);
+    var remoteThread = await RemoteThread.init(threadName);
     return new SyncedThread(localThread, remoteThread);
   }
 
-  async getOrCreateCollection<T extends Instance>(collectionName: string, schema: JSONSchema): Promise<SyncedCollection<T>>
-  {
+  async getOrCreateCollection<T extends Instance>(collectionName: string, schema: JSONSchema): Promise<SyncedCollection<T>> {
     if (!await this.localThread.hasCollection(collectionName))
       await this.localThread.createCollection(collectionName, schema);
 
-    this.remoteThread.then(thread =>
-    {
-      thread.hasCollection(collectionName).then(async (result) =>
-      {
-        if (!result)
-          await thread.createCollection(collectionName, schema)
-      });
+    this.remoteThread.hasCollection(collectionName).then(async (result) => {
+      if (!result)
+        await this.remoteThread.createCollection(collectionName, schema)
     });
 
     return await this.getCollection(collectionName);
   }
 
-  async getCollection<T extends Instance>(collectionName: string): Promise<SyncedCollection<T>>
-  {
+  async getCollection<T extends Instance>(collectionName: string): Promise<SyncedCollection<T>> {
     return await SyncedCollection.init<T>(collectionName, this.localThread, this.remoteThread);
   }
 }
