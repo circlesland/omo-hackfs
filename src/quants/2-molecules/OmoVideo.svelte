ff383285c29b4a20f49cd9536f112a3dbd2a40c0
Mama Omo <mama@omo.earth>
2020-08-07 05:52:59 +0200

    uploaded video

--- src/quants/2-molecules/OmoVideo.svelte
+++ src/quants/2-molecules/OmoVideo.svelte
@@ -1,6 +1,6 @@
 <script>
   export const data = {
-    image: ""
+    image: "",
   };
   import OmoModal from "./OmoModal.svelte";
 
@@ -22,35 +22,35 @@
 <OmoModal {triggerRef} bind:isOpen>
   <video controls>
     <source
-            src="https://ipfs.io/ipfs/QmQn4Wih8PYHBzrkq55y4gnrhe2z9SYtYnsLrJHGenNTXC"
-            type="video/mp4"/>
+      src="https://ipfs.io/ipfs/bafybeiftyawil7ddly7x46nqxoex6o5h624at3g37toyzkyy5tunnrdykq"
+      type="video/mp4" />
     Your browser does not support the video tag.
   </video>
 </OmoModal>
 
 <section
-        class="font-sans h-full w-full bg-cover text-center flex flex-col items-center
+  class="font-sans h-full w-full bg-cover text-center flex flex-col items-center
   justify-center"
-        style="background:url(https://source.unsplash.com/featured/?city) no-repeat
+  style="background:url(https://source.unsplash.com/featured/?city) no-repeat
   center;">
 
   <div
-          class="h-full w-full bg-cover text-center flex flex-col items-center
+    class="h-full w-full bg-cover text-center flex flex-col items-center
     justify-center bg-primary opacity-50"
-          bind:this={triggerRef}
-          on:click={() => (isOpen = !isOpen)}>
+    bind:this={triggerRef}
+    on:click={() => (isOpen = !isOpen)}>
     <div
-            class="bg-white text-black hover:bg-secondary rounded-full h-16 w-16 flex
+      class="bg-white text-black hover:bg-secondary rounded-full h-16 w-16 flex
       items-center justify-center mb-8">
-      <i class="fas fa-play ml-1 "/>
+      <i class="fas fa-play ml-1 " />
     </div>
     <label
-            for=""
-            class="text-white uppercase tracking-extrawide text-xs font-hairline">
+      for=""
+      class="text-white uppercase tracking-extrawide text-xs font-hairline">
       Watch Video
     </label>
     <h3
-            class="text-white mx-auto max-w-lg mt-4 font-normal text-2xl
+      class="text-white mx-auto max-w-lg mt-4 font-normal text-2xl
       leading-normal">
       Our dream is to empower the emerging universal basic income economy of
       tomorrow
